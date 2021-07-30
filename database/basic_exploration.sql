4c9935dc9106c5ae67f36d346e2c912f6df52d09
Rafael Arias <rafael.ariascalles@gmail.com>
2021-07-30 04:31:31 -0600

    <code> Aditional information for model building

--- database/basic_exploration.sql
+++ database/basic_exploration.sql
@@ -16,7 +16,7 @@ left join noc_unit nu  on md.unit_code  = nu.unit_code
 -- Main dataset for recommendation using duties as query and titel as collaborate filter
 
 select 
-md.text_description duties, nue.text_description title , nu.unit_title noc_name,nu.unit_code noc 
+md.text_description job_duties, nue.text_description job_title , nu.unit_title noc_name,nu.unit_code noc 
 from public.noc_unit_main_duties md
 left join noc_unit nu  on md.unit_code  = nu.unit_code
 left join noc_unit_example nue on md.unit_code  = nue.unit_code 
